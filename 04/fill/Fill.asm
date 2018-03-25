fd1b560930d39218a34048e45d4ced08e6fd0b43
Damien Rochford <djrochford@gmail.com>
2018-03-25 16:24:08 -0400

    Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,34 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(OUTER_LOOP)
+  @SCREEN
+  D=A
+  @current_word
+  M=D
+  (INNER_LOOP)
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ
+    (BLACK)
+      @current_word
+      A=M
+      M=-1
+      @GET_NEXT_WORD
+      0;JMP
+    (WHITE)
+      @current_word
+      A=M
+      M=0
+    (GET_NEXT_WORD)
+      @current_word
+      D=M
+      @24576
+      D=A-D
+      @OUTER_LOOP
+      D;JEQ 
+      @current_word
+      M=M+1
+      @INNER_LOOP
+      0;JMP
