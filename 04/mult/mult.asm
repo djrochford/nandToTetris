72bb2a85e0ea11fc69cf31ef7722d6a37918a3aa
Damien Rochford <djrochford@gmail.com>
2018-03-25 12:45:18 -0400

    Mult assembly and binary code.

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @i
+  M=0
+  @R2
+  M=0
+(LOOP)
+  @i
+  D=M
+  @R1
+  D=M-D
+  @END
+  D;JLE
+  @R2
+  D=M
+  @R0
+  D=D+M
+  @R2
+  M=D
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
+
+
